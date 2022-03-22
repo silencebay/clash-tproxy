7e9ecae0f30efe05f4f33b9b2e7a54f93f837e22
silencebay <silencebay@users.noreply.github.com>
2022-03-22 14:39:36 +0000

    fixed tproxy with fake-ip

--- Dockerfile
+++ Dockerfile
@@ -30,6 +30,7 @@ ARG TARGETPLATFORM
 ARG BUILDPLATFORM
 ARG FIREQOS_VERSION=latest
 ARG FIREQOS_TARGET_COMMITISH
+ENV FAKE_IP_RANGE=198.18.0.1/16
 
 # RUN echo "https://mirror.tuna.tsinghua.edu.cn/alpine/v3.11/main/" > /etc/apk/repositories
 
--- scripts/setup-tproxy.sh
+++ scripts/setup-tproxy.sh
@@ -3,7 +3,7 @@
 source /usr/lib/clash/common.sh
 
 # > ROUTE RULES
-ip rule add fwmark "$PROXY_FWMARK" table 100
+ip rule add fwmark "${PROXY_FWMARK}" table 100
 ip route add local default dev lo table 100
 
 # > 本机流量
@@ -18,8 +18,9 @@ iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
 iptables -t mangle -A CLASH -p udp --dport 53 -j RETURN
 # >> prevent zerotier redirect
 iptables -t mangle -A CLASH -p udp --dport 9993 -j RETURN
-iptables -t mangle -A CLASH -p udp -j TPROXY --on-port 7893 --tproxy-mark "$PROXY_FWMARK"
-iptables -t mangle -A CLASH -p tcp -j TPROXY --on-port 7893 --tproxy-mark "$PROXY_FWMARK"
+# [ "${EN_MODE:-fake-ip}" = "fake-ip" ] && iptables -t mangle -A CLASH -d "${FAKE_IP_RANGE}" -j MARK --set-mark $PROXY_FWMARK
+iptables -t mangle -A CLASH -p udp -j TPROXY --on-port 7893 --tproxy-mark "${PROXY_FWMARK}"
+iptables -t mangle -A CLASH -p tcp -j TPROXY --on-port 7893 --tproxy-mark "${PROXY_FWMARK}"
 # >> REDIRECT
 iptables -t mangle -A PREROUTING -j CLASH
 
@@ -31,19 +32,22 @@ iptables -t mangle -A CLASH_MASK -m set --match-set localnetwork dst -j RETURN
 iptables -t mangle -A CLASH_MASK -d 255.255.255.255/32 -j RETURN
 iptables -t mangle -A CLASH_MASK -p udp --dport 53 -j RETURN
 iptables -t mangle -A CLASH_MASK -p udp --dport 9993 -j RETURN
-iptables -t mangle -A CLASH_MASK -m owner --uid-owner "$PROXY_BYPASS_USER" -j RETURN
+iptables -t mangle -A CLASH_MASK -m owner --uid-owner "${PROXY_BYPASS_USER}" -j RETURN
 iptables -t mangle -A CLASH_MASK -j RETURN -m mark --mark 0xff
-iptables -t mangle -A CLASH_MASK -p udp -j MARK --set-mark "$PROXY_FWMARK"
-iptables -t mangle -A CLASH_MASK -p tcp -j MARK --set-mark "$PROXY_FWMARK"
+# [ "${EN_MODE:-fake-ip}" = "fake-ip" ] && iptables -t mangle -A CLASH_MASK -d "${FAKE_IP_RANGE}" -j MARK --set-mark $PROXY_FWMARK
+iptables -t mangle -A CLASH_MASK -p udp -j MARK --set-mark "${PROXY_FWMARK}"
+iptables -t mangle -A CLASH_MASK -p tcp -j MARK --set-mark "${PROXY_FWMARK}"
 # >> REDIRECT OUTPUT CHAIN
 iptables -t mangle -A OUTPUT -j CLASH_MASK
 
 # > 新建 DIVERT 规则，避免已有连接的包二次通过 TPROXY，理论上有一定的性能提升
 # 同时解决无法访问已接管的私有地址(如：不在 localnetwork 中的地址) 的问题
-iptables -t mangle -N DIVERT
-iptables -t mangle -A DIVERT -j MARK --set-mark "$PROXY_FWMARK"
-iptables -t mangle -A DIVERT -j ACCEPT
-iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
+if [ "${EN_MODE:-fake-ip}" = "redir-host" ]; then
+    iptables -t mangle -N DIVERT
+    iptables -t mangle -A DIVERT -j MARK --set-mark "${PROXY_FWMARK}"
+    iptables -t mangle -A DIVERT -j ACCEPT
+    iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
+fi
 
 # > LOCAL MACHINE DNS
 log "[DNS] Setting local machine dns"
